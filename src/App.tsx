e0d4b8c3141502853e0f9ccd03af8865c71ab2af
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-03-29 00:26:26 +0300

    Revert "what"
    This reverts commit 2a53dc3b53ff84f50302ea66bbc710b19bd071fa.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+import "./App.css";
+import Quest from "./Quest";
+
+function App() {
+  return <Quest />;
+}
+
+export default App;
