a1ef998d6ef8a0c5179fdee4094c06a66cd0dffa
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-03-28 21:52:15 +0300

    awd

--- backend/built/emailTemplates/signupreq.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (key) => "<h1>You will need this Key (never give it to anyone):</h1><p>" +
-    key +
-    "</p>";
