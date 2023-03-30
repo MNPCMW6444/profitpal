10f8bdd0fff8c2e418062f9e82f431124e8dcfd7
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-03-31 00:57:50 +0300

    qwe

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -37,7 +37,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5999",
-      "https://profitpal.flexboxtorchy.com",
+      "https://caphub.flexboxtorchy.com",
       "https://app.profitpal.com",
     ],
     credentials: true,
--- src/util/domain.ts
+++ src/util/domain.ts
@@ -1,3 +1,3 @@
 export default process.env.NODE_ENV === "development"
   ? "http://localhost:6555/"
-  : "https://coinpal.onrender.com/";
+  : "https://caphub.onrender.com/";
