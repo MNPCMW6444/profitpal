580f63862eaf34d57f0d47d5a12a21dd5db066e7
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-03-30 11:59:39 +0300

    good

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,11 +12,16 @@ dotenv.config();
 let mainDbStatus = false;
 
 const connectToDBs = () => {
-  mongoose.connect("" + process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as ConnectOptions);
-
+  try {
+    mongoose.connect("" + process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
+    mainDbStatus = true;
+  } catch (e) {
+    console.error(e);
+    mainDbStatus = false;
+  }
   if (!mainDbStatus) setTimeout(connectToDBs, 180000);
   else console.log("connected to mongo");
 };
@@ -27,7 +32,11 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5999"],
+    origin: [
+      "http://localhost:5999",
+      "https://profitpal.flexboxtorchy.com",
+      "https://app.profitpal.com",
+    ],
     credentials: true,
   })
 );
