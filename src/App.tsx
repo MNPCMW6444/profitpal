7c3d1e445ecbf64e5032bbe98365589f1392d3d0
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-03-31 00:46:39 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import { Box, Typography, Button, Grid, Container } from "@mui/material";
 import { useState } from "react";
 import POC from "./components/POC";
-import { Box, Typography, Button } from "@mui/material";
 
 const App = () => {
   const [x, setX] = useState("");
@@ -12,34 +12,41 @@ const App = () => {
       <POC />
     )
   ) : (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      minHeight="100vh"
-    >
-      <Typography variant="h4" gutterBottom>
-        Are you a founder or a VC manager?
-      </Typography>
-      <Box mt={2}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => setX("vcs")}
-          style={{ marginRight: "16px" }}
-        >
-          VC Manager
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => setX("founders")}
-        >
-          Founder
-        </Button>
+    <Container>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        minHeight="100vh"
+      >
+        <Typography variant="h4" align="center" gutterBottom>
+          Are you a founder or a VC manager?
+        </Typography>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={12} sm={6} md={4}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setX("vcs")}
+              fullWidth
+            >
+              VC Manager
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setX("founders")}
+              fullWidth
+            >
+              Founder
+            </Button>
+          </Grid>
+        </Grid>
       </Box>
-    </Box>
+    </Container>
   );
 };
 export default App;
