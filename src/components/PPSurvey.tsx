e22092e061211b8c3350132dc16eb241ed7cdb6f
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-03-31 15:24:25 +0300

    Revert "rtg43t"
    This reverts commit ba7b484c286301ba7c4048b86e602095d4acf19c.

--- src/components/PPSurvey.tsx
+++ src/components/PPSurvey.tsx
@@ -1,29 +1,20 @@
 import "survey-core/defaultV2.min.css";
-import { useCallback, useContext, useState } from "react";
+import { useCallback, useContext } from "react";
 import { Survey } from "survey-react-ui";
 import { Model } from "survey-core";
 import axios from "axios";
 import UserContext from "../context/UserContext";
-import ProgressBarWithText from "./ProgressBarWithText";
 
 export default function PPSurvey({ surveyJson, type }: any) {
   const { user } = useContext(UserContext);
-  const [show, showP] = useState(false);
-  const [data, saveData] = useState();
   const survey = new Model(surveyJson);
   const surveyComplete = useCallback(
     ({ data }: any) => {
-      saveData(data);
-
+      debugger;
       axios.post("/save" + type, { owner: user, data });
     },
     [user]
   );
   survey.onComplete.add(surveyComplete);
-  survey.onComplete.add(() => showP(true));
-  return !show ? (
-    <Survey model={survey} />
-  ) : (
-    <ProgressBarWithText founder={data} />
-  );
+  return <Survey model={survey} />;
 }
